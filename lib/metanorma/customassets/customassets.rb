99685ab2389ceb393a62b28ea6251a8dfe90495b
Nick Nicholas <opoudjis@gmail.com>
2025-06-02 21:07:26 +1000

    i18n of doctype, ICC: https://github.com/metanorma/metanorma-iso/issues/1356

--- lib/metanorma/customassets/customassets.rb
+++ lib/metanorma/customassets/customassets.rb
@@ -11,11 +11,13 @@ module Metanorma
     def process_input_adoc_overrides(attrs, options)
       case @flavor
       when :icc
-        f = File.join(File.dirname(__FILE__), 'assets', 'icc-boilerplate.adoc')
-        [":boilerplate-authority: #{f}",
-         ':publisher: International Color Consortium',
-         ':publisher_abbr: ICC'].each { |a| attrs << a }
-        options[':boilerplate-authority:'] = f
+        f1 = File.join(File.dirname(__FILE__), "assets", "icc-boilerplate.adoc")
+        f2 = File.join(File.dirname(__FILE__), "assets", "icc-i18n.yaml")
+        [":boilerplate-authority: #{f1}",
+         ":i18nyaml: #{f2}",
+         ":publisher: International Color Consortium",
+         ":publisher_abbr: ICC"].each { |a| attrs << a }
+        options[":boilerplate-authority:"] = f
       end
       attrs
     end
