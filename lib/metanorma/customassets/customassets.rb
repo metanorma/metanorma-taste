d7d0a2977468deb49e2f8dbb633aa36a63cf341c
Nick Nicholas <opoudjis@gmail.com>
2025-06-02 21:17:12 +1000

    init

--- lib/metanorma/customassets/customassets.rb
+++ lib/metanorma/customassets/customassets.rb
@@ -9,15 +9,21 @@ module Metanorma
     end
 
     def process_input_adoc_overrides(attrs, options)
+      # Insert after the second element (index 1)
+      # If attrs has fewer than 2 elements, this will handle it appropriately
+      insertion_index = [attrs.length, 2].min
       case @flavor
       when :icc
         f1 = File.join(File.dirname(__FILE__), "assets", "icc-boilerplate.adoc")
         f2 = File.join(File.dirname(__FILE__), "assets", "icc-i18n.yaml")
-        [":boilerplate-authority: #{f1}",
-         ":i18nyaml: #{f2}",
-         ":publisher: International Color Consortium",
-         ":publisher_abbr: ICC"].each { |a| attrs << a }
-        options[":boilerplate-authority:"] = f
+        new_attrs = [
+          ":boilerplate-authority: #{f1}",
+          ":i18nyaml: #{f2}",
+          ":publisher: International Color Consortium",
+          ":publisher_abbr: ICC",
+        ]
+        attrs.insert(insertion_index, *new_attrs)
+        options[":boilerplate-authority:"] = f1
       end
       attrs
     end
